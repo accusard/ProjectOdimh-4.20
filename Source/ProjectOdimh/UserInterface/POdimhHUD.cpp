04719a84489b7d316417e1e6a55252a08fe6c684
Vanny Sou <accusard@me.com>
2019-10-02 15:31:04 -0700

    Remove unused functions

--- Source/ProjectOdimh/UserInterface/POdimhHUD.cpp
+++ Source/ProjectOdimh/UserInterface/POdimhHUD.cpp
@@ -1,43 +1,6 @@
 // Copyright 2017-2018 Vanny Sou. All Rights Reserved.
 
 #include "POdimhHUD.h"
-#include "Gametypes.h"
-#include "Kismet/GameplayStatics.h"
-#include "UserInterface/POdimhWidget.h"
 
-UPOdimhWidget* APOdimhHUD::CreateWidgetFromBlueprint(TSubclassOf<UPOdimhWidget> WidgetBlueprint)
-{
-    UPOdimhWidget* WidgetPtr = nullptr;
-    
-    // if widget blueprint exists
-    if(WidgetBlueprint)
-    {
-        APlayerController* Player = UGameplayStatics::GetPlayerController(GetWorld(), (int32)EPlayer::One);
-        WidgetPtr = CreateWidget<UPOdimhWidget>(Player, WidgetBlueprint);
-    }
-    
-    return WidgetPtr;
-}
 
-void APOdimhHUD::SetActiveWidget(UUserWidget* Widget)
-{
-    if(Widget)
-    {
-        ActiveWidget = Widget;
-        ActiveWidget->AddToViewport();
-    }
-    else
-    {
-        UE_LOG(LogTemp,Warning,TEXT("No active blueprint available to create widget!"));
-    }
-}
 
-UPOdimhWidget* APOdimhHUD::GetMenuSelectionWidget()
-{
-    return MenuSelection;
-}
-
-void APOdimhHUD::BeginPlay()
-{
-    Super::BeginPlay();
-}
--- Source/ProjectOdimh/UserInterface/POdimhHUD.h
+++ Source/ProjectOdimh/UserInterface/POdimhHUD.h
@@ -19,25 +19,8 @@ class PROJECTODIMH_API APOdimhHUD : public AHUD
 	GENERATED_BODY()
 	
 public:
-    // TODO: widget is created in blueprint.. make an event to get its reference?
-    /** Setup function to create player 1's widget from blueprint */
-    UPOdimhWidget* CreateWidgetFromBlueprint(TSubclassOf<UPOdimhWidget> WidgetBlueprint);
-    
-    /** Set the current active widget so that the HUD can have access to it */
-    void SetActiveWidget(UUserWidget* Widget);
-    
-    /** Get the currently active widget */
-    UFUNCTION(BlueprintCallable)
-    UPOdimhWidget* GetMenuSelectionWidget();
+
     
 protected:
-    void BeginPlay() override;
-    
-    /** Set blueprint for the main menu UI */
-    UPROPERTY(EditDefaultsOnly, Category="Widget")
-    TSubclassOf<UPOdimhWidget> MenuBlueprint;
-    UPOdimhWidget* MenuSelection;
-    
-    /** The pointer to the currently active widget */
-    UUserWidget* ActiveWidget;
+
 };
